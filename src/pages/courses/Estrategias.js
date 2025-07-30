c7ff9ca8ceb33e1b6d6c8fb23ed1f755edce3bbd
Ilan Daniele <ilan.daniele@gmail.com>
2025-07-30 11:47:07 -0300

    correcting z on marzzo

--- src/pages/courses/Estrategias.js
+++ src/pages/courses/Estrategias.js
@@ -68,7 +68,7 @@ const Estrategias = () => {
 
         <div className="bg-gray-800 p-4 rounded-lg mb-6">
           <h3 className="text-xl font-bold text-yellow-400 mb-2">1️⃣ Grupo Visionario</h3>
-          <ul className="list-disc list-inside text-gray-300 space-y-1">
+          <ul className="list-none text-gray-300 space-y-1">
             <li>🗓️ Del 14 de Septiembre al 13 de Noviembre de 2025</li>
             <li>💸 Valor: 39 dólares</li>
             <li>🎁 Bonus: mentoría grupal en agosto + PDF "Sexo Sagrado" + acceso completo a PDFs Diálogos GM</li>
